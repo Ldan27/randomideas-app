73fb9a2c98c37c42b9d22c192aeb32dfa8fb3753
MWITWA KATANTI DAN <danielkatanti@gmail.com>
2024-11-20 14:40:37 +0200

    updated

--- routes/ideas.js
+++ routes/ideas.js
@@ -43,4 +43,51 @@ router.get('/:id', (req, res) => {
   res.json({ success: true, data: idea });
 });
 
+// Update idea
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({ success: true, data: idea });
+});
+
+// Delete idea
+router.delete('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1);
+
+  res.json({ delete: true, data: idea.id + ' was deleted' });
+});
+
+// Add an idea
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+
+  ideas.push(idea);
+
+  res.send(req.body.text);
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const port = 5000;
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json()); /** allow us to send raw data to the server */
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeas API' });
 });
