29cb3a6224656a9fc8e73bb5c1a6b32712fa9e8b
MWITWA KATANTI DAN <danielkatanti@gmail.com>
2024-11-26 06:58:21 +0200

    commit everthing

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
 
@@ -8,9 +10,20 @@ const startServer = async () => {
     await connectDB();
     const app = express();
 
+    // Static folder
+    app.use(express.static(path.join(__dirname, 'public')));
+
     // Body parser middleware
     app.use(express.json()); /** allow us to send raw data to the server */
     app.use(express.urlencoded({ extended: false }));
+
+    // cors middleware
+    app.use(
+      cors({
+        origin: ['http://localhost:5000', 'http://localhost:3000'],
+        credentials: true,
+      })
+    );
 
     app.get('/', (req, res) => {
       res.json({ message: 'Welcome to the RandomIdeas API' });
